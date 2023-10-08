c0e9490d9fc03bab01ce5d153b1f49028234fa6b
Ding <advancedor96@gmail.com>
2023-10-08 21:32:47 +0800

    add feature

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -4,26 +4,23 @@
   <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea
-            class="textarea"
-            placeholder="Add a new note"
-          />
+          <textarea v-model="newNote" ref="newNoteRef" class="textarea" placeholder="Add a new note" />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button class="button is-link has-background-success" @click="addNote" :disabled="!newNote">
             Add New Note
           </button>
         </div>
       </div>
     </div>
 
-    <div v-for="i in 3" :key="i" class="card mb-4" >
+    <div v-for="note in notes" :key="note.id" class="card mb-4" >
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.
+          {{  note.content }}
         </div>
       </div>
       <footer class="card-footer">
@@ -33,3 +30,36 @@
     </div>
 </div>
 </template>
+<script setup>
+import {ref} from 'vue'
+const newNote = ref('')
+const newNoteRef = ref(null)
+
+const addNote = ()=>{
+  console.log('yoo');
+
+  let currentDate = new Date().getTime(), 
+      id = currentDate.toString();
+  let note = {
+    id: id,
+    content: newNote.value
+  }
+  notes.value.unshift(note);
+  newNote.value = ""
+  newNoteRef.value.focus();
+  
+
+  
+}
+const notes = ref([
+  {
+    id: 'id1',
+    content: 'Lorem'
+  },
+  {
+    id: 'id2',
+    content: 'Lorem2'
+  },
+])
+
+</script>
