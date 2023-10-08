9d5978556fd96a117a742f4d22fe4dff9e46f98e
Ding <advancedor96@gmail.com>
2023-10-08 21:59:25 +0800

    update

---
+++ src/components/Notes/Note.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card mb-4" >
+    <div class="card-content">
+      <div class="content">
+        {{  props.note.content }} 
+      </div>
+      <div class="has-text-right has-text-grey-light">
+        <small>{{ contentLength }} </small>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  note: {
+    type: Object
+  } 
+})
+const contentLength = computed(()=>{
+  if(props.note.content.length ===1) return '1 char'
+  else return `${props.note.content.length} chars`  
+})
+console.log('props.content', props.note);
+
+</script>
